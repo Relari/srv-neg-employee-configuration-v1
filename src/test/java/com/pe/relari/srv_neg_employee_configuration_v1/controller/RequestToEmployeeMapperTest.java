8319bec768eac42e007e1dbf538b8b39233b96d9
Relari <renzolavador@gmail.com>
2024-09-06 17:13:17 -0500

    Updated unit test

--- src/test/java/com/pe/relari/srv_neg_employee_configuration_v1/controller/RequestToEmployeeMapperTest.java
+++ src/test/java/com/pe/relari/srv_neg_employee_configuration_v1/controller/RequestToEmployeeMapperTest.java
@@ -3,7 +3,7 @@ package com.pe.relari.srv_neg_employee_configuration_v1.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.pe.relari.srv_neg_employee_configuration_v1.employee.model.api.EmployeeRequest;
-import com.pe.relari.srv_neg_employee_configuration_v1.employee.model.api.PersonResponse;
+import com.pe.relari.srv_neg_employee_configuration_v1.employee.model.api.EmployeeResponse;
 import com.pe.relari.srv_neg_employee_configuration_v1.employee.model.domain.Employee;
 import com.pe.relari.srv_neg_employee_configuration_v1.employee.util.TestMapper;
 
@@ -26,9 +26,9 @@ class RequestToEmployeeMapperTest {
   void mapPersonResponse() {
 
     Employee employee = TestMapper.employee();
-    PersonResponse personResponse = RequestToEmployeeMapper.mapPersonResponse(employee);
+    EmployeeResponse employeeResponse = RequestToEmployeeMapper.mapPersonResponse(employee);
 
-    assertEquals(employee.getFirstName(), personResponse.getFirstName());
+    assertEquals(employee.getFirstName(), employeeResponse.getFirstName());
   }
 
 }
